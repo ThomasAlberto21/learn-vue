12d65f28cb71daebc302f0eb5a25798a81d1f918
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-10 20:49:57 +0700

    BIND

--- src/components/1.Binding/BindingText.vue
+++ src/components/1.Binding/BindingText.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1 class="text-green-500 text-2xl font-semibold">Hello My Name Is {{ name }}</h1>
+  <!-- Cara 1 menggunakan double curly braces -->
+  <h1 class="text-green-500 text-2xl font-semibold">
+    Hello My Name Is {{ name }}
+  </h1>
+
+  <!-- Cara 2 menggunakan v-text -->
+  <p v-text="nim" class="text-green-500 text-2xl font-semibold"></p>
 </template>
 
 <script>
@@ -8,6 +14,7 @@ export default {
   data() {
     return {
       name: 'Thomas Alberto',
+      nim: '12221355',
     };
   },
 };
