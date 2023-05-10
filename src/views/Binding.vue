c65f736893149665c273b41a7baa4226dde34eec
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-10 20:44:47 +0700

    BINDING

--- src/views/Binding.vue
+++ src/views/Binding.vue
@@ -1,3 +1,6 @@
+<!-- Todo -->
+<!-- Binding di Vue.js merujuk pada cara untuk menghubungkan atau mengikat data dari objek Vue ke elemen HTML. Dengan binding, perubahan yang dilakukan pada data di dalam objek Vue akan secara otomatis memperbarui tampilan di dalam elemen HTML yang terkait. -->
+
 <template>
   <BindingText />
 </template>
