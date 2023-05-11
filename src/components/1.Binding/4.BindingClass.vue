ebe889b115ef41e95da4fb1479d9fa0d2b8957df
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-11 21:04:41 +0700

    .

--- src/components/1.Binding/4.BindingClass.vue
+++ src/components/1.Binding/4.BindingClass.vue
@@ -3,7 +3,10 @@
 
 <template>
   <div>
-    <h1 v-bind:class="status">Underline</h1>
+    <h1 v-bind:class="status">Cara 1</h1>
+
+    <!-- Jika isPromoted true maka jalankan class promoted dan jika isPromoted false maka jangan jalankan isPromoted nya -->
+    <h2 v-bind:class="isPromoted && 'promoted'">Cara 2</h2>
   </div>
 </template>
 
@@ -12,7 +15,15 @@ export default {
   data() {
     return {
       status: 'text-green-400 font-bold text-2xl',
+      isPromoted: true,
     };
   },
 };
 </script>
+
+<style>
+.promoted {
+  color: aqua;
+  font-size: x-large;
+}
+</style>
