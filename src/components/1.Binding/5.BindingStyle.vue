f1987b68895252a01f46967a2a5918a62194b8f2
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-11 21:51:15 +0700

    BINDING STYLE

--- src/components/1.Binding/5.BindingStyle.vue
+++ src/components/1.Binding/5.BindingStyle.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <!-- Memasukkan object css yang sudah dibuat di komponen Vue js dan memasukkan ke dalam HTML -->
+  <div v-bind:style="[border, padding]">
     <!-- Memasukkan inline style yang sudah dibuat di komponen vue dan memasukkan nya ke dalam HTML -->
     <h1
       v-bind:style="{
@@ -11,7 +12,7 @@
       Inline Style
     </h1>
 
-    <!-- Memasukkan object css sekaligus dalam 1 komponen vue js dan memasukkan ke dalam HTML -->
+    <!-- Memasukkan object css sekaligus yang sudah dibuat di komponen Vue js dan memasukkan ke dalam HTML -->
     <h1 v-bind:style="styleObject">Inline Style</h1>
   </div>
 </template>
@@ -27,7 +28,24 @@ export default {
         fontSize: '50px',
         padding: '20px',
       },
+
+      border: {
+        border: '2px solid green',
+      },
+
+      padding: {
+        paddingTop: '20px',
+        paddingBottom: '20px',
+        paddingLeft: '20px',
+        paddingRight: '20px',
+      },
     };
   },
 };
 </script>
+
+<!-- <style>
+*{
+    pad
+}
+</style> -->
