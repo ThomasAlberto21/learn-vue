4ed58428f8e983cdfa3a9c24d993ec52ba11760a
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-05-12 19:53:26 +0700

    .

--- src/components/2.Conditional/1.ConditionalRendering.vue
+++ src/components/2.Conditional/1.ConditionalRendering.vue
@@ -3,11 +3,31 @@
 
 <template>
   <div>
-    <!-- Ini cara pembuatan kodisi di vue js , dimana jika number sama dengan 0 maka tampilkan HTML nya dan jika tidak maka jangan tampilkan HTML nya -->
     <h1 v-if="number === 0" class="text-green-400 font-semibold text-2xl">
-      Ini adalah nomor 0
+      This is number 0
     </h1>
+
+    <h1 v-else-if="number > 0" class="text-green-400 font-semibold text-2xl">
+      The number positive
+    </h1>
+
+    <h1 v-else-if="number < 0" class="text-green-400 font-semibold text-2xl">
+      The number negative
+    </h1>
+
+    <h1 v-else class="text-green-400 font-semibold text-2xl">Is not number</h1>
   </div>
+
+  <!-- Template tag dalam conditional rendering di Vue.js berguna untuk mengelompokkan beberapa elemen HTML atau komponen Vue.js dalam satu blok dan diterapkan juga kondisi pada blok tersebut  -->
+  <template v-if="shodHeader">
+    <h1 class="text-green-400 font-semibold text-2xl">Ini adalah Header</h1>
+    <p class="text-orange-400 text-lg">Ini adalah text header</p>
+  </template>
+
+  <template v-if="showFooter">
+    <h1 class="text-green-400 font-semibold text-2xl">Ini adalah Footer</h1>
+    <p class="text-orange-400 text-lg">Ini adalah text footer</p>
+  </template>
 </template>
 
 <script>
@@ -15,6 +35,8 @@ export default {
   data() {
     return {
       number: 0,
+      shodHeader: false,
+      showFooter: true,
     };
   },
 };
